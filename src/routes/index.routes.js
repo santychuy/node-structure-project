690d9a88970dbe0f50fdb59954c9b42569ea5baa
Jesús Santiago Carrasco Campa <jsanticc@hotmail.com>
2019-10-11 21:21:45 -0700

    - Done

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.send('Hello');
+});
+
 router.get('/easter/fuckin/egg', (req, res) => {
     res.send('Node project structure');
 });
